45381dbfeceb99178a1c69223b68d818199a5dc0
Dayo-Akinsola <dayoakinsola3@gmail.com>
2021-08-18 22:31:55 +0100

    Random colors show on hover instead of just red

--- etchASketch.js
+++ etchASketch.js
@@ -1,9 +1,8 @@
 const container = document.querySelector('.container');
-let squares = container.querySelectorAll('.square');
 
 // Creates a intxint grid
 const createGrid = (int) => {
-    squares = document.querySelectorAll('.square');
+    const squares = document.querySelectorAll('.square');
     squares.forEach(square => square.remove());
     for (let i = 0; i < int*int; i++){
         let div = document.createElement('div');
@@ -19,37 +18,53 @@ const createGrid = (int) => {
 let dimensionsChoice = 16;
 createGrid(dimensionsChoice);
 
-
-//Changes the color of a box when hovered.
-const colorChange = () =>{
-    squares = document.querySelectorAll('.square');
-    squares.forEach(square => square.addEventListener('mouseover', (event) => {
-    event.target.classList.add("redTransition");
-    console.log(event.target)
-}))
-}
-colorChange();
-
+// Resets the colours back to white on the current grid
 const resetGrid = () => {
     const resetButton = document.querySelector('#resetButton');
+    const squares = document.querySelectorAll('.square');
     resetButton.addEventListener('click', (event) => {
-        squares.forEach(square => square.classList.remove('redTransition'))
-        console.log(event.target)
+        squares.forEach(square => square.classList.remove('randomColor'))
     })
 }
 resetGrid();
 
-const dimensionsButton = document.querySelector('#gridDimensions');
 
+//Changes the color of a box when hovered.
+const colorChangeBlack = () =>{
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', (event) => {
+    event.target.classList.add("blackTransition");
+}))
+}
+
+const getRandomColor = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return '#' + randomColor;
+}
+
+const colorChangeRandom = () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', (event) => {
+        let randomColor = getRandomColor();
+        event.target.classList.add("randomColor");
+        event.target.style.backgroundColor = randomColor;
+    }))
+}
+
+const dimensionsButton = document.querySelector('#gridDimensions');
 dimensionsButton.addEventListener('click', (event) => {
     dimensionsChoice = prompt('Please enter a number between 2 - 100.');
-    while (parseInt(dimensionsChoice) > 100 || parseInt(dimensionsChoice) < 2){
+    while (parseInt(dimensionsChoice) > 100 || parseInt(dimensionsChoice) < 2 || isNaN(dimensionsChoice)){
         dimensionsChoice = prompt('Please enter an number between 2 - 100.')
     }
-    createGrid(parseInt(dimensionsChoice));
-
+    if (dimensionsChoice){
+        createGrid(parseInt(dimensionsChoice));
+    }
+    colorChangeRandom();
+    resetGrid();
 })
 
+colorChangeRandom();
 
 
 
